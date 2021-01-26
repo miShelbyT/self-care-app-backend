8fb68b8d8d9d000d54173593fa186ccd18d1c95a
miShelbyT <mi.shelbyrose@gmail.com>
2021-01-26 09:20:44 -0500

    added journal entry validation to keep code from breaking due to submission of empty journal entry form

--- app/models/journal_entry.rb
+++ app/models/journal_entry.rb
@@ -1,4 +1,9 @@
 class JournalEntry < ApplicationRecord
   belongs_to :user
   belongs_to :user_activity
+
+  validates :date, presence: true
+  validates :length_of_time, presence: true
+  validates :comments, presence: true
+
 end
